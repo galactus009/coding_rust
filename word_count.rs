7a3097d202900efb7bda008a65eda2b503617433
ayalar <Hellobhaskar@icloud.com>
2022-01-30 15:29:46 +0530

    Improved

--- word_count.rs
+++ word_count.rs
@@ -1,22 +1,22 @@
-
+#[warn(unused_imports)]
 use std::collections::HashMap;
 
-fn word_count(phrase: &'static str) -> HashMap<String,i32> {
-	let mut d:HashMap<String,i32> = HashMap::new();
-	for word in phrase.split(' ') {
-	   if d.contains_key(word) {
-	    	d.insert(word.to_string(),d.get(word).unwrap()+1);
-        } else {
-	    d.insert(word.to_string(),1);
-	}
-	}
-	d
+/// Count occurrences of words.
+pub fn word_count(words: &str) -> HashMap<String, u32> {
+    let mut d: HashMap<String, u32> = HashMap::new();
+    for mut word in words.split(' ').map(|s| s.to_string()).filter(|c| !c.is_empty()) {
+        word.retain(|c| c.is_alphanumeric());
+	*d.entry(word.to_lowercase().clone()).or_insert(0)+=1;
+    }
+    d
 }
 
+
 fn main() {
-  let phrase = "man That's the password: 'PASSWORD 123'!, cried the Special Agent.\nSo I fled. silly man";
-  let res:HashMap<String,i32>=word_count(&phrase);
-  for (key,val) in res.iter(){
-	println!("{} -> {} ",key,val);	
-  }
+    let phrase ="man That's the password: 'PASSWORD 123'!, cried the Special Agent.\nSo I fled. silly man";
+    println!("{} ",phrase);
+    let res: HashMap<String, u32> = word_count(&phrase);
+    for (key, val) in res.iter() {
+        println!("{} -> {} ", key, val);
+    }
 }
